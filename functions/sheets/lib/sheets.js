88331c392a46b0bf551f6e3e651e48f3d8a06d42
Daniel Jhin Yoo <danieljyoo@gmail.com>
2019-07-07 15:56:46 -0700

    update sheets func with Auth0 integration and org based filtering

--- functions/sheets/lib/sheets.js
+++ functions/sheets/lib/sheets.js
@@ -1,17 +1,60 @@
 async function sheetsHandler(ctx) {
-  ctx.response.json(await getSheetsForOrg(ctx.state.mongodb))
+  let user = userInfo(ctx)
+  if (!user) {
+    ctx.response.httperror(401, 'Unauthorized')
+    return
+  }
+  let db = ctx.state.mongodb
+  ctx.response.json(await getSheetsForOrg(db, user))
   return
 }
 
 // Hack: For now we just return all sheets in the db! Meta doesn't store
 // any info about the org or the creater/updater of the sheet.
-async function getSheetsForOrg(db, id) {
-  let query = { }
+async function getSheetsForOrg(db, user) {
+  let query = getQuery(user)
   let options = {
     sort: [ [ 'title', 1 ] ],
     projection: { id: 1, title: 1, updated_at: 1, uuid: 1, nrows: 1 }
   }
   return await db.collection('spreadsheets').find(query, options).toArray()
+}
+
+function getQuery(user) {
+  let query = { }
+  if (!user.org) {
+    // just get sheets the user is the creator of
+    query = { "created_by": user.userid }
+  } else if (user.org == "goalbookapp.com") {
+    // match on goalbookapp.com and 
+    query = {
+      "$or": [ 
+        { "created_by_org": user.org }, 
+        { "created_by_org": { "$exists": false } } 
+      ]
+    }
+  } else {
+    // match only on user.org
+    query = { "created_by_org": user.org }
+  }
+  return query
+}
+
+function userInfo(ctx) {
+  let auth = ctx.state.auth
+  if (!auth || !auth.success) {
+    return null
+  }
+  let decoded = ctx.state.auth.decoded
+  let userid = decoded.sub
+  let email = decoded.email && decoded.email.toLowerCase() || null
+  let org = getOrgFromEmail(email)
+  return { userid, email, org }
+}
+
+function getOrgFromEmail(email) {
+  if (!email || email.endsWith("@gmail.com")) return null
+  return email.split('@')[1]
 }
 
 module.exports = {
--- functions/sheets/test/sheets.test.js
+++ functions/sheets/test/sheets.test.js
@@ -6,6 +6,29 @@ const prettify = require('@funcmaticjs/pretty-logs')
 // Goalbook Fist to Five Backend
 const GOOGLESPREADSHEET_ID = "1liBHwxOdE7nTonL1Cv-5hzy8UGBeLpx0mufIq5dR8-U"
 
+describe('Error Handling', () => {
+  let func = null
+  beforeEach(async () => {
+    func = require('../index.js').func
+    func.logger.logger.prettify = prettify
+  })
+  afterEach(async () => {
+    await func.invokeTeardown()
+  })
+  it ("should return 401 Unauthorized if user is unauthenticated", async () => {
+    let ctx = createCtx() 
+    delete ctx.event.headers['Authorization']
+    await func.invoke(ctx)
+    expect(ctx.response).toMatchObject({
+      statusCode: 401
+    })
+    let body = JSON.parse(ctx.response.body)
+    expect(body).toMatchObject({
+      "errorMessage": "Unauthorized"
+    })
+  })
+})
+
 describe('Function', () => {
   let func = null
   beforeEach(async () => {
@@ -15,37 +38,38 @@ describe('Function', () => {
   afterEach(async () => {
     await func.invokeTeardown()
   })
-  it ("should fetch all sheets in the db (for now)", async () => {
+  it ("should fetch all sheets in the db for goalbookapp.com users", async () => {
     let ctx = createCtx() 
     await func.invoke(ctx)
     expect(ctx.response).toMatchObject({
       statusCode: 200
     })
     let body = JSON.parse(ctx.response.body)
-    // console.log(JSON.stringify(body, null, 2))
     expect(body.length).toBe(15)
     expect(body[1]).toMatchObject({
-      "_id": "5d1adabce62b51923ec06260",
       "id": "1liBHwxOdE7nTonL1Cv-5hzy8UGBeLpx0mufIq5dR8-U",
       "title": "Goalbook Fist to Five Backend",
-      "updated_at": "2019-07-02T04:35:04.413Z",
-      "uuid": "a4938a00-2573-4173-9c24-c3f1c81c18b2",
+      "updated_at": expect.anything(),
       "nrows": 6
     })
   })
 })
-
 
 function createCtx() {
   return { 
     event: {
       httpMethod: 'GET',
       headers: {
-        'Content-Type': 'application/json'
+        'Content-Type': 'application/json',
+        'Authorization': process.env.AUTH0_TOKEN
       },
       stageVariables: {
         FUNC_PARAMETERSTORE_PATH: '/supersheetsio/dev'
       }
+    },
+    env: {
+      FUNC_AUTH0_DOMAIN: process.env.FUNC_AUTH0_DOMAIN,
+      FUNC_AUTH0_SKIP_VERIFICATION: 'true'
     }
   }
 }
