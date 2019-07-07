5070bc07770e3914841587a8ddf0c349edb12e92
Daniel Jhin Yoo <danieljyoo@gmail.com>
2019-07-07 13:17:03 -0700

    Updated get function with Auth0

--- functions/get/lib/get.js
+++ functions/get/lib/get.js
@@ -1,10 +1,50 @@
 async function getHandler(ctx) {
-  ctx.response.json(await getMetadata(ctx.state.mongodb, ctx.event.pathParameters.spreadsheetid))
+  let user = userInfo(ctx)
+  if (!user) {
+    ctx.response.httperror(401, 'Unauthorized')
+    return
+  }
+  let db = ctx.state.mongodb
+  let id = ctx.event.pathParameters.spreadsheetid
+  let metadata = null
+  try {
+    metadata = await getMetadata(db, id)
+  } catch (err) {
+    ctx.response.httperror(500, `Error looking up metadata for ${id}`, { expose: true })
+    return
+  } 
+  if (!metadata) {
+    ctx.response.httperror(404, `Could not find metadata with id ${id}`)
+    return
+  }
+  if (metadata.created_by_org && metadata.created_by_org != user.org) {
+    // if there is a org then it has to match the user's
+    ctx.response.httperror(401, 'Unauthorized')
+    return
+  }
+  ctx.response.json(metadata)
   return
 }
 
 async function getMetadata(db, id) {
   return await db.collection('spreadsheets').findOne({ id })
+}
+
+function userInfo(ctx) {
+  let auth = ctx.state.auth
+  if (!auth || !auth.success) {
+    return null
+  }
+  let decoded = ctx.state.auth.decoded
+  let userid = decoded.sub
+  let email = decoded.email && decoded.email.toLowerCase() || null
+  let org = getOrgFromEmail(email)
+  return { userid, email, org }
+}
+
+function getOrgFromEmail(email) {
+  if (!email || email.endsWith("@gmail.com")) return null
+  return email.split('@')[1]
 }
 
 module.exports = {
--- functions/get/test/get.test.js
+++ functions/get/test/get.test.js
@@ -6,7 +6,7 @@ const prettify = require('@funcmaticjs/pretty-logs')
 // Goalbook Fist to Five Backend
 const GOOGLESPREADSHEET_ID = "1liBHwxOdE7nTonL1Cv-5hzy8UGBeLpx0mufIq5dR8-U"
 
-describe('Function', () => {
+describe('Error Handling', () => {
   let func = null
   beforeEach(async () => {
     func = require('../index.js').func
@@ -15,18 +15,85 @@ describe('Function', () => {
   afterEach(async () => {
     await func.invokeTeardown()
   })
-  it ("should fetch and update sheet metadata", async () => {
+  it ("should return 401 Unauthorized if there if user is unauthenticated", async () => {
+    let ctx = createCtx() 
+    delete ctx.event.headers['Authorization']
+    await func.invoke(ctx)
+    expect(ctx.response).toMatchObject({
+      statusCode: 401
+    })
+    let body = JSON.parse(ctx.response.body)
+    expect(body).toMatchObject({
+      errorMessage: "Unauthorized"
+    })
+  })
+  it ("should return 401 Unauthorized if the user belongs to a different org", async () => {
+    let ctx = createCtx() 
+    ctx.state = { 
+      mongodb: mockdb(async () => {
+        return { created_by_org: "some-other-org" }
+      }) 
+    }
+    await func.invoke(ctx)
+    expect(ctx.response).toMatchObject({
+      statusCode: 401
+    })
+    let body = JSON.parse(ctx.response.body)
+    expect(body).toMatchObject({
+      errorMessage: "Unauthorized"
+    })
+  })
+  it ("should return 404 Not Found if the id is invalid", async () => {
+    let ctx = createCtx() 
+    ctx.event.pathParameters.spreadsheetid = 'BAD-SPREADSHEET-ID'
+    await func.invoke(ctx)
+    expect(ctx.response).toMatchObject({
+      statusCode: 404
+    })
+    let body = JSON.parse(ctx.response.body)
+    expect(body).toMatchObject({
+      errorMessage: `Could not find metadata with id BAD-SPREADSHEET-ID` 
+    })
+  })
+  it ("should return 500 Internal Server error if mongodb error", async () => {
+    let ctx = createCtx() 
+    ctx.state = { 
+      mongodb: mockdb(async () => {
+        throw new Error("some Mongodb error")
+      }) 
+    }
+    await func.invoke(ctx)
+    expect(ctx.response).toMatchObject({
+      statusCode: 500
+    })
+    let body = JSON.parse(ctx.response.body)
+    expect(body).toMatchObject({
+      errorMessage: `Error looking up metadata for ${GOOGLESPREADSHEET_ID}`
+    })
+  })
+})
+
+describe('Function', () => { 
+  let func = null
+  beforeEach(async () => {
+    func = require('../index.js').func
+    func.logger.logger.prettify = prettify
+  })
+  afterEach(async () => {
+    await func.invokeTeardown()
+  })
+  it ("should return the metadata for a spreadsheet", async () => {
     let ctx = createCtx() 
     await func.invoke(ctx)
     expect(ctx.response).toMatchObject({
       statusCode: 200
     })
     let body = JSON.parse(ctx.response.body)
-    console.log(JSON.stringify(body, null, 2))
-    //expect(body).toMatchObject()
+    expect(body).toMatchObject({
+      id: GOOGLESPREADSHEET_ID
+    })
   })
 })
-
 
 function createCtx() {
   return { 
@@ -36,10 +103,28 @@ function createCtx() {
         spreadsheetid: GOOGLESPREADSHEET_ID
       },
       headers: {
-        'Content-Type': 'application/json'
+        'Content-Type': 'application/json',
+        'Authorization': process.env.AUTH0_TOKEN
       },
       stageVariables: {
-        FUNC_PARAMETERSTORE_PATH: '/supersheetsio/dev'
+        // FUNC_PARAMETERSTORE_PATH: '/supersheetsio/dev'
+        // staging for now while /dev is still being used in production
+        FUNC_PARAMETERSTORE_PATH: '/supersheetsio/staging'
+      },
+      env: {
+        FUNC_AUTH0_SKIP_VERIFICATION: 'true'
+      }
+    }
+  }
+}
+
+function mockdb(callback) {
+  return {
+    collection: () => {
+      return {
+        findOne: async () => {
+          return await callback()
+        }
       }
     }
   }
