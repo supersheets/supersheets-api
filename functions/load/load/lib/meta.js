3368de8b209c4e8b7340b8a01c9ccb25227ae2b2
Daniel Jhin Yoo <danieljyoo@gmail.com>
2019-08-26 21:23:59 -0700

    loader refactor: initial cut of meta logic which needs more testing

---
+++ functions/load/load/lib/meta.js
@@ -0,0 +1,127 @@
+const uuidV4 = require('uuid/v4')
+const COLLECTION = 'spreadsheets'
+const IGNORE_PREFIX = "_"
+
+async function metaHandler(ctx, next) {
+  await initOrFindMetadata(ctx)
+  await fetchAndMergeMetadata(ctx)
+  await next()
+  await createOrUpdateMetadata(ctx)
+}
+
+async function initOrFindMetadata(ctx) {
+  if (ctx.state.metadata) return await next()
+  let id = ctx.event.body.spreadsheetid
+  if (!id) {
+    throw new Error("No Google Spreadsheet Doc ID provided ('ctx.event.body.spreadsheetid')")
+  }
+  let db = ctx.state.mongodb
+  let user = ctx.state.user
+  let metadata = await findMetadata(db, id)
+  if (metadata) {
+    if (metadata.created_by_org && metadata.created_by_org != user.org) {
+      throw new Error(`Unauthorized: ${user.email} not does not belong to org ${metadata.created_by_org}`)
+    }
+  } 
+  if (!metadata) {
+    metadata = { "_new": true }
+  }
+  ctx.state.metadata = metadata
+  return
+}
+
+async function fetchAndMergeMetadata(ctx) {
+  let id = ctx.event.body.spreadsheetid
+  let doc = (await ctx.state.sheetsapi.get(id)).data
+  let latest = createMetadataFromGoogleSpreadsheet(doc)
+  Object.assign(ctx.state.metadata, latest)
+}
+
+function createMetadataFromGoogleSpreadsheet(doc) {
+  let metadata = {
+    id: doc.spreadsheetId,
+    url: doc.spreadsheetUrl,
+    title: doc.properties.title,
+    tz: doc.properties.timeZone,
+    local: doc.properties.locale, // TYPO but keep things backward compatible
+    locale: doc.properties.locale
+  }
+  let sheets = [ ]
+  
+  for (var i=0; i<doc.sheets.length; i++) {
+    let sheetDoc = doc.sheets[i].properties
+    if (!sheetDoc.title.startsWith(IGNORE_PREFIX)) {
+      sheets.push({
+        id: sheetDoc.sheetId,
+        title: sheetDoc.title,
+        index: sheetDoc.index,
+        sheetType: sheetDoc.sheetType
+      })
+    }
+  }
+  metadata.sheets = sheets
+  return metadata
+}
+
+async function createOrUpdateMetadata(ctx) {
+  let user = ctx.state.user
+  let metadata = ctx.state.metadata
+  let db = ctx.state.mongodb
+  if (metadata["_new"]) {
+    Object.assign(metadata, metadataCreateFields(user))
+    delete metadata["_new"]
+  } else {
+    if (!metadata.created_at) {
+      // Old v1 metadata will not have created_* fields
+      Object.assign(metadata, metadataCreateFields(user))
+    }
+    Object.assign(metadata, metadataUpdateFields(user))
+  }
+  await saveMetadata(db, metadata)
+  ctx.state.metadata = await findMetadata(db, metadata.id)
+  return
+}
+
+function metadataCreateFields(user) {
+  return {
+    uuid: uuidV4(),
+    created_at: new Date(),
+    created_by: user.userid,
+    created_by_email: user.email,
+    created_by_org: user.org
+  }
+}
+
+function metadataUpdateFields(user) {
+  return {
+    updated_at: new Date(),
+    updated_by: user.userid,
+    updated_by_email: user.email,
+    updated_by_org: user.org
+  }
+}
+
+async function findMetadata(db, id) {
+  return await db.collection(COLLECTION).findOne({ id })
+}
+
+async function saveMetadata(db, metadata) {
+  let id = metadata.id
+  return await db.collection(COLLECTION).updateOne({ id }, { "$set": metadata }, { upsert: true, w: 1 })
+}
+
+// unit testing only
+async function deleteMetadata(db, id) {
+  return await db.collection(COLLECTION).deleteOne({ id })
+}
+
+module.exports = {
+  metaHandler,
+  initOrFindMetadata,
+  createOrUpdateMetadata,
+  fetchAndMergeMetadata,
+  createMetadataFromGoogleSpreadsheet,
+  findMetadata,
+  saveMetadata,
+  deleteMetadata
+}
---
+++ functions/load/load/test/meta.test.js
@@ -0,0 +1,279 @@
+require('dotenv').config()
+const uuidV4 = require('uuid/v4')
+const MongoDBPlugin = require('@funcmaticjs/mongodb-plugin')
+const { LoggerWrapper } = require('@funcmaticjs/funcmatic')
+const prettify = require('@funcmaticjs/pretty-logs')
+const awsParamStore = require('aws-param-store')
+const axios = require('axios')
+
+// Supersheets Public GraphQL Test
+// https://docs.google.com/spreadsheets/d/1hCmRdgeWAnPEEzK-GHKdJDNjRZdhUHaKQKJ2IX7fTVI/edit#gid=0
+const GOOGLESPREADSHEET_ID = "1hCmRdgeWAnPEEzK-GHKdJDNjRZdhUHaKQKJ2IX7fTVI"
+
+//const GOOGLESPREADSHEET_PRIVATE_ID = "1JYT2HbToNeafKuODTW-gdwvJwmZ0MRYCqibRzZOlfJY"
+// Goalbook Private Supersheets Cross Domain Test
+// Added service account email with edit access to this sheet
+// https://docs.google.com/spreadsheets/d/1UWbjiyx0gL9tsbsKoYUeeCoiwdefNyka4Rn00NFX-pM/edit#gid=0
+const GOOGLESPREADSHEET_PRIVATE_ID = "1UWbjiyx0gL9tsbsKoYUeeCoiwdefNyka4Rn00NFX-pM"
+
+const { 
+  findStatus,
+  updateStatus,
+  deleteStatus
+} = require('../lib/status')
+
+const {
+  metaHandler,
+  initOrFindMetadata,
+  initOrFetchMetadata,
+  fetchAndMergeMetadata,
+  createOrUpdateMetadata,
+  findMetadata,
+  saveMetadata,
+  deleteMetadata
+} = require('../lib/meta')
+
+const NOOP = async () => { }
+
+describe('initOrFindMeta', () => {
+  let plugin = new MongoDBPlugin()
+  let client = null
+  let db = null
+  let meta = null
+  let status = null
+  beforeAll(async () => {
+    client = await plugin.createClient(process.env.FUNC_MONGODB_URI)
+    db = client.db()
+  })
+  afterAll(async () => {
+    if (client) {
+      await client.close()
+    }
+    client = null
+    db = null
+  })
+  beforeEach(async () => {
+    meta = await createTestMetadata(db)
+    status = await createTestStatus(db)
+  })
+  afterEach(async () => {
+    await deleteStatus(db, { uuid: status.uuid })
+    await deleteMetadata(db, { id: meta.id })
+    status = null
+  })
+  it ('should throw if spreadsheetid not in the body', async () => {
+    let ctx = createTestCtx({
+      mongodb: db
+    })
+    ctx.event.body.spreadsheetid = null
+    let error = null
+    try {
+      await initOrFindMetadata(ctx)
+    } catch (err) {
+      error = err
+    }
+    expect(error).toBeTruthy()
+    expect(error.message).toEqual("No Google Spreadsheet Doc ID provided ('ctx.event.body.spreadsheetid')")
+  })
+  it ('should throw if user does not belong to org', async () => {
+    let ctx = createTestCtx({
+      mongodb: db,
+      user: { email: "user@someother.org", org: "someother.org" }
+    })
+    let error = null
+    try {
+      await initOrFindMetadata(ctx)
+    } catch (err) {
+      error = err
+    }
+    expect(error).toBeTruthy()
+    expect(error.message).toEqual(`Unauthorized: user@someother.org not does not belong to org funcmatic.com`)
+  })
+  it ('should find and set existing metadata', async () => {
+    let ctx = createTestCtx({
+      spreadsheetid: meta.id,
+      mongodb: db
+    })
+    await initOrFindMetadata(ctx)
+    console.log(ctx.state.metadata)
+    expect(ctx.state.metadata).toMatchObject({
+      id: meta.id
+    })
+  })
+  it ('should initialize an empty metadata', async () => {
+    let ctx = createTestCtx({
+      spreadsheetid: 'new-spreadsheet-id',
+      mongodb: db
+    })
+    await initOrFindMetadata(ctx)
+    console.log(ctx.state.metadata)
+    expect(ctx.state.metadata).toMatchObject({
+      "_new": true
+    })
+  })
+})
+
+describe('fetchAndMergeMetadata', () => {
+  let sheetsapi = null
+  beforeAll(async () => {
+    let token = (await awsParamStore.getParameter(process.env.FUNC_GOOGLE_SERVICE_ACCOUNT_TOKEN_PATH)).Value
+    sheetsapi = axios.create({
+      baseURL: process.env.GOOGLESHEETS_BASE_URL
+    })
+    sheetsapi.defaults.headers.common['Authorization'] = `Bearer ${token}`
+  })
+  afterAll(async () => {
+  })
+  // it ('should throw if spreadsheetid not in the body', async () => {
+  //   let ctx = createTestCtx({
+  //     mongodb: db
+  //   })
+  //   ctx.event.body.spreadsheetid = null
+  //   let error = null
+  //   try {
+  //     await initOrFindMetadata(ctx)
+  //   } catch (err) {
+  //     error = err
+  //   }
+  //   expect(error).toBeTruthy()
+  //   expect(error.message).toEqual("No Google Spreadsheet Doc ID provided ('ctx.event.body.spreadsheetid')")
+  // })
+  it ('should fetch and merge metadata from a public sheet', async () => {
+    let ctx = createTestCtx({
+      spreadsheetid: GOOGLESPREADSHEET_ID
+    })
+    ctx.state.metadata = { "_new": true }
+    ctx.state.sheetsapi = sheetsapi
+    await fetchAndMergeMetadata(ctx)
+    expect(ctx.state.metadata).toMatchObject({
+      "_new": true,
+      id: GOOGLESPREADSHEET_ID,
+      title: 'Supersheets Public GraphQL Test'
+    })
+  })
+  it ('should fetch and merge metadata from a private sheet', async () => {
+    let ctx = createTestCtx({
+      spreadsheetid: GOOGLESPREADSHEET_PRIVATE_ID
+    })
+    ctx.state.metadata = { "_new": true }
+    ctx.state.sheetsapi = sheetsapi
+    await fetchAndMergeMetadata(ctx)
+    expect(ctx.state.metadata).toMatchObject({
+      "_new": true,
+      id: GOOGLESPREADSHEET_PRIVATE_ID,
+      title: 'Goalbook Private Supersheets Cross Domain Test'
+    })
+  })
+})
+
+describe('createOrUpdateMeta', () => {
+  let plugin = new MongoDBPlugin()
+  let client = null
+  let db = null
+  let meta = null
+  let status = null
+  beforeAll(async () => {
+    client = await plugin.createClient(process.env.FUNC_MONGODB_URI)
+    db = client.db()
+  })
+  afterAll(async () => {
+    if (client) {
+      await client.close()
+    }
+    client = null
+    db = null
+  })
+  beforeEach(async () => {
+    meta = await createTestMetadata(db)
+    status = await createTestStatus(db)
+  })
+  afterEach(async () => {
+    await deleteStatus(db, { uuid: status.uuid })
+    await deleteMetadata(db, { id: meta.id })
+    status = null
+  })
+  // it ('should throw if spreadsheetid not in the body', async () => {
+  //   let ctx = createTestCtx({
+  //     mongodb: db
+  //   })
+  //   ctx.event.body.spreadsheetid = null
+  //   let error = null
+  //   try {
+  //     await initOrFindMetadata(ctx)
+  //   } catch (err) {
+  //     error = err
+  //   }
+  //   expect(error).toBeTruthy()
+  //   expect(error.message).toEqual("No Google Spreadsheet Doc ID provided ('ctx.event.body.spreadsheetid')")
+  // })
+  // TODO: need to test create vs update
+  // also test backward compat if no created_at fields when doing an update
+  // also ensure that if existing db in database it is overwritten by this
+  it ('should find and set existing metadata', async () => {
+    let ctx = createTestCtx({
+      mongodb: db
+    })
+    ctx.state.metadata = {
+      id: meta.id
+    }
+    await createOrUpdateMetadata(ctx)
+    console.log(ctx.state.metadata)
+    expect(ctx.state.metadata).toMatchObject({
+      id: meta.id
+    })
+  })
+})
+
+function createTestUser() {
+  return {
+    userid: "1234",
+    email: "danieljyoo@funcmatic.com",
+    org: "funcmatic.com"
+  }
+}
+
+async function createTestMetadata(db, options) {
+  options = options || { }
+  let user = options.user || createTestUser()
+  let metadata = {
+    id: options.id || GOOGLESPREADSHEET_ID,
+    created_by_org: user.org
+  }
+  await saveMetadata(db, metadata)
+  return findMetadata(db, metadata.id)
+}
+
+
+async function createTestStatus(db, options) {
+  options = options || { }
+  let user = options.user || createTestUser()
+  let status = {
+    uuid: options.uuid || uuidV4(),
+    status: options.status || "INIT"
+  }
+  try {
+    await updateStatus(db, { uuid: status.uuid }, { "$set": status })
+  } catch (err) {
+    console.error(err)
+  }
+  return status
+}
+
+function createTestCtx(options) {
+  options = options || { }
+  let user = options.user || createTestUser()
+  return { 
+    event: { 
+      body: {
+        spreadsheetid: options.spreadsheetid || GOOGLESPREADSHEET_ID
+      }
+    },
+    env: { },
+    state: { 
+      mongodb: options.mongodb,
+      user
+    },
+    error: options.error || null,
+    logger: new LoggerWrapper({ prettify })
+  }
+}
