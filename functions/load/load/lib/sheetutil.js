4ff12ca138ac48afd774afc187e7f305fe8355c3
Daniel Jhin Yoo <danieljyoo@gmail.com>
2019-08-30 23:09:17 -0700

    load refactor: moved conversion code out of sheetutil

--- functions/load/load/lib/sheetutil.js
+++ functions/load/load/lib/sheetutil.js
@@ -1,13 +1,8 @@
-const uuidV4 = require('uuid/v4')
-const { DateTime } = require('luxon')
 const IGNORE_PREFIX = "_"
+const GRAPHQL_NAME_REGEX = /^[_A-Za-z][_0-9A-Za-z]*$/
 
-function getLoadMode(metadata) {
-  return metadata.config && metadata.config.mode || 'FORMATTED'
-}
-
-function getAccess(metadata) {
-  return metadata.config && metadata.config.access || 'public'
+function isValidColumnName(name) {
+  return name && !name.startsWith(IGNORE_PREFIX) && name.match(GRAPHQL_NAME_REGEX) && true || false
 }
 
 function constructDocs(sheetDoc, data) {
@@ -25,226 +20,16 @@ function constructDocs(sheetDoc, data) {
       };
       for (var j = 0; j < cols.length; j++) {
         let column = cols[j]
-        if (!column.startsWith(IGNORE_PREFIX)) {
+        if (isValidColumnName(column)) {
           doc[cols[j]] = data[i][j]
         }
       }
       docs.push(doc)
     }
   }
-  // We have to do this at the end so it doesn't throw off indexing (i, j)
-  cols = cols.filter(name => !name.startsWith(IGNORE_PREFIX))
-  return { cols, docs }
-}
-
-function convertValues(cols, docs, datatypes, options) {
-  let conv = createConverter(datatypes)
-  options = options || { }
-  for (let doc of docs) {
-    for (let col of cols) {
-      try {
-        if (conv[col]) {
-          doc[col] = conv[col](doc[col], options)
-        }
-      } catch (err) {
-        doc["_errors"].push({
-          col, message: err.message
-        })
-      }
-    }
-  }
-  return { cols, docs }
-}
-
-function createConverter(datatypes) {
-  let conv = { }
-  for (let col in datatypes) {
-    switch(datatypes[col]) {
-      case "Boolean":
-        conv[col] = convertToBoolean
-        break
-      case "String":
-        conv[col] = convertToString
-        break
-      case "Number":
-        conv[col] = convertToNumber
-        break
-      case "Int":
-        conv[col] = convertToInt
-        break
-      case "Float":
-        conv[col] = convertToNumber
-        break
-      case "Date":
-        conv[col] = convertToDate
-        break
-      case "Datetime":
-        conv[col] = convertToDatetime
-        break
-      case "JSON":
-        conv[col] = convertToJSON
-        break
-      case "StringList":
-        conv[col] = convertToStringList
-        break
-      case "GoogleDoc":
-        // we just noop because we'll have separate
-        // logic that will deal with Google Docs
-        conv[col] = convertNoop
-        break
-      default:
-        throw new Error(`Unknown type ${datatypes[col]}`)
-    }
-  }
-  return conv
-}
-
-const convertToString = (v) => {
-  if (!v) return ''
-  switch(typeof v) {
-    case "string":
-      return v
-    case "number":
-      return v.toString()
-    case "object":
-      return JSON.stringify(v)
-    default:
-      throw new Error(`Could not convert value ${v} to type String`)
-  }
-}
-
-const convertToNumber = (v) => {
-  if (!v) return 0
-  switch(typeof v) {
-    case "number": 
-      return v
-    case "string": 
-      return new Number(v)
-    default:
-      throw new Error(`Could not convert value ${v} to type Number`)
-  }
-}
-
-const convertToInt = (v) => {
-  if (!v) return 0
-  switch(typeof v) {
-    case "number": 
-      return Math.floor(v)
-    case "string": 
-      return parseInt(v)
-    default:
-      throw new Error(`Could not convert value ${v} to type Int`)
-  }
-}
-
-
-const convertToBoolean = (v) => {
-  if (!v) return false
-  return true
-}
-
-const convertToDatetime = (v, options) => {
-  options = options || { }
-  let tz = options.tz || "utc"
-  if (v === null) return null
-  switch(typeof v) {
-    case "number": 
-      return getISOStringFromExcel(v, tz)
-    case "string": 
-      let fmt = options.fmt || 'ISO' 
-      return parseDatetimeFromString(v, fmt, tz)
-    default:
-      throw new Error(`Could not convert value ${v} to type Date`)
-  }
-}
-
-// Will use the month, day, year and store in UTC WITHOUT tz converstion
-// For example, if the the sheet's timezone is
-// the value is the equivalent of '2009-04-01'('America/Los_Angeles') 
-// what will be stored is 'Z' (UTC)
-const convertToDate = (v, options) => {
-  options = options || { }
-  let tz = "utc"
-  if (v === null) return null
-  switch(typeof v) {
-    case "number": 
-      return getISODateStringFromExcel(v, tz)
-    case "string": 
-      let fmt = options.fmt || 'ISO' 
-      return parseDateFromString(v, fmt, tz)
-    default:
-      throw new Error(`Could not convert value ${v} to type Date`)
-  }
-}
-
-const convertToJSON = (v) => {
-  switch (typeof v) {
-    case "string":
-      return JSON.parse(v)
-    case "number": 
-      return JSON.parse(v)
-    case "object":
-      return JSON.parse(JSON.stringify(v))
-    default:
-      throw new Error(`Could not convert value ${v} to JSON`)
-  }
-}
-
-const convertToStringList = (v, options) => {
-  options = options || { }
-  let separator = options.separator || '\n'
-  if (!v) return [ ]
-  switch (typeof v) {
-    case "string":
-      return v.split(separator).map(s => s.trim()).filter(s => s)
-    case "number":
-      return [ v.toString() ]
-    default:
-      throw new Error(`Could not convert value ${v} to StringList`)
-  }
-}
-
-const convertNoop = (v) => {
-  return v
-}
-
-function getISOStringFromExcel(excelDate, tz) {
-  let d = getJsDateFromExcel(excelDate)
-  let str = d.toISOString().slice(0, -1) // remove 'Z' so we have a tz agnostic date
-  // return DateTime.fromISO(str, { zone: tz })
-  let datetime = DateTime.fromISO(str, { zone: tz })  // we have the literal time in the local timezone (the spreadsheet)
-  return datetime.setZone('utc') // we convert local time to utc
-}
-
-function getISODateStringFromExcel(excelDate, tz) {
-  let d = getJsDateFromExcel(excelDate)
-  let str = d.toISOString().split('T')[0] // 2018-04-05
-  return DateTime.fromISO(str, { zone: tz })
-}
-
-function getJsDateFromExcel(excelDate) { 
-  return new Date(Math.round((excelDate - (25567 + 2))*86400)*1000) 
-}
-
-function parseDatetimeFromString(str, fmt, tz) {
-  if (fmt == "ISO") {
-    // setZone only sets the zone if str does not include offset
-    return DateTime.fromISO(str, { zone: tz, setZone: true })
-  }
-  return DateTime.fromFormat(str, fmt, { zone: tz })
-}
-
-function parseDateFromString(str, fmt, tz) {
-  if (fmt == "ISO") {
-    // setZone only sets the zone if str does not include offset
-    str = str.split('T')[0]
-    return DateTime.fromISO(str, { zone: tz, setZone: true }).set({
-      hour: 0, minute: 0, seconds: 0, millisecond: 0 
-    })
-  }
-  return DateTime.fromFormat(str, fmt, { zone: tz }).set({
-    hour: 0, minute: 0, seconds: 0, millisecond: 0 
-  })
+  cols = cols.filter(name => isValidColumnName(name))
+  let excluded = cols.filter(name => !isValidColumnName(name))
+  return { cols, docs, excluded } 
 }
 
 function updateSpreadsheetCountsFromSheets(metadata) {
@@ -276,87 +61,79 @@ function updateSheetDoc(sheet, docs) {
   return sheet
 }
 
-function getSampleColumnValues(cols, docs) {
-  let samples = { }
-  for (let name of cols) {
-    samples[name] = null
-    for (let doc of docs) {
-      if (doc[name]) {
-        samples[name] = doc[name]
-        break
-      }
-    }
-  }
-  return samples
-}
-
-function updateSchema(metadata) {
-  let datatypes = metadata.config && metadata.config.mode == "UNFORMATTED" && metadata.config.datatypes || { }
-  if (!metadata.schema) {
-    metadata.schema = { }
-  }
-  metadata.schema.columns = [ ]
-  let schema = metadata.schema
-  let cols = { }
-  for (let sheet of metadata.sheets) {
-    for (let column of sheet.columns) {
-      if (!cols[column]) {
-        schema.columns.push({
-          name: column,
-          datatype: datatypes[column] || "String",
-          sample: sheet.samples[column],
-        })
-        cols[column] = [ sheet.title ]
-      } else {
-        cols[column].push(sheet.title)
-      }
-    }
-  }
-  for (col of schema.columns) {
-    col.sheets = cols[col.name]
-  }
-  // metadata.schema = schema
-  return metadata 
-}
-
-function updateGoogleDocSchemas(metadata, schemas) {
-  if (!metadata.schema) {
-    metadata.schema = { }
-  }
-  if (!metadata.schema.docs) {
-    metadata.schema.docs = schemas
-    return
-  }
-  for (let col in schemas) {
-    if (metadata.schema.docs[col]) {
-      updateExistingGoogleDocSchema(metadata.schema.docs[col], schemas[col])
-    } else {
-      metadata.schema.docs[col] = schemas[col]
-    }
-  }
-}
-
-function updateExistingGoogleDocSchema(current, schema) {
-  for (let key of schema.columns) {
-    if (!current.columns.find(k => k.name == key.name)) {
-      current.columns.push(key)
-    }
-  }
-}
-
-
 module.exports = {
-  getLoadMode,
-  getAccess,
   updateSpreadsheetCountsFromSheets,
   constructDocs,
-  convertValues,
-  updateSheetDoc,
-  getSampleColumnValues,
-  updateSchema,
-  createConverter,
-  updateGoogleDocSchemas
+  updateSheetDoc
 }
+
+// function getSampleColumnValues(cols, docs) {
+//   let samples = { }
+//   for (let name of cols) {
+//     samples[name] = null
+//     for (let doc of docs) {
+//       if (doc[name]) {
+//         samples[name] = doc[name]
+//         break
+//       }
+//     }
+//   }
+//   return samples
+// }
+
+// function updateSchema(metadata) {
+//   let datatypes = metadata.config && metadata.config.mode == "UNFORMATTED" && metadata.config.datatypes || { }
+//   if (!metadata.schema) {
+//     metadata.schema = { }
+//   }
+//   metadata.schema.columns = [ ]
+//   let schema = metadata.schema
+//   let cols = { }
+//   for (let sheet of metadata.sheets) {
+//     for (let column of sheet.columns) {
+//       if (!cols[column]) {
+//         schema.columns.push({
+//           name: column,
+//           datatype: datatypes[column] || "String",
+//           sample: sheet.samples[column],
+//         })
+//         cols[column] = [ sheet.title ]
+//       } else {
+//         cols[column].push(sheet.title)
+//       }
+//     }
+//   }
+//   for (col of schema.columns) {
+//     col.sheets = cols[col.name]
+//   }
+//   // metadata.schema = schema
+//   return metadata 
+// }
+
+// function updateGoogleDocSchemas(metadata, schemas) {
+//   if (!metadata.schema) {
+//     metadata.schema = { }
+//   }
+//   if (!metadata.schema.docs) {
+//     metadata.schema.docs = schemas
+//     return
+//   }
+//   for (let col in schemas) {
+//     if (metadata.schema.docs[col]) {
+//       updateExistingGoogleDocSchema(metadata.schema.docs[col], schemas[col])
+//     } else {
+//       metadata.schema.docs[col] = schemas[col]
+//     }
+//   }
+// }
+
+// function updateExistingGoogleDocSchema(current, schema) {
+//   for (let key of schema.columns) {
+//     if (!current.columns.find(k => k.name == key.name)) {
+//       current.columns.push(key)
+//     }
+//   }
+// }
 
 // function detectNewColumns(dataObject, sheetDoc) {
 //   var fields = Object.keys(dataObject).filter(isUserField);
--- functions/load/load/test/sheetutil.test.js
+++ functions/load/load/test/sheetutil.test.js
@@ -1,36 +1,11 @@
 require('dotenv').config()
-const sheetutil = require('../lib/sheetutil')
-const { DateTime } = require('luxon')
-
-describe('Schema', () => {
-  beforeEach(async () => {
-  })
-  it ('should get samples from docs', async () => {
-    let { cols, docs } = createTestData()
-    let samples = sheetutil.getSampleColumnValues(cols, docs)
-    expect(samples).toMatchObject({
-      "col1": "val1a",
-      "col2": "val2b",
-      "col3": null
-    })
-  })
-  it ('should create schema from columns and samples', async () => {
-    let metadata = createTestMetadata()
-    sheetutil.updateSchema(metadata)
-    expect(metadata.schema.columns.length).toBe(4)
-    expect(metadata.schema.columns.map(col => col.name)).toEqual([ "A", "B", "C", "D" ])
-    expect(metadata.schema.columns.map(col => col.sample)).toEqual([ "A1", "B1", null, "D2" ])
-    expect(metadata.schema.columns[0].sheets).toEqual([ "Sheet1" ])
-    expect(metadata.schema.columns[1].sheets).toEqual([ "Sheet1", "Sheet2" ])
-    expect(metadata.schema.columns[2].sheets).toEqual([ "Sheet1" ])
-    expect(metadata.schema.columns[3].sheets).toEqual([ "Sheet2" ])
-  })
-})
+const { constructDocs } = require('../lib/sheetutil')
+const { convertValues } = require('../lib/convert')
 
 describe('Construct Docs', () => {
   let sheetDoc = { title: "title" }
   it ('should set formatted data', async () => {
-    let { cols, docs } = sheetutil.constructDocs(sheetDoc, formattedData)
+    let { cols, docs } = constructDocs(sheetDoc, formattedData)
     expect(docs[0]).toMatchObject({
       "String": "Hello",
       "Number": "3",
@@ -38,7 +13,7 @@ describe('Construct Docs', () => {
     })
   })
   it ('should set unformatted data', async () => {
-    let { cols, docs } = sheetutil.constructDocs(sheetDoc, unformattedData)
+    let { cols, docs } = constructDocs(sheetDoc, unformattedData)
     expect(docs[0]).toMatchObject({
       "String": "Hello",
       "Number": 3,
@@ -51,8 +26,8 @@ describe('Construct Docs', () => {
       Number: "Number",
       Datetime: "Datetime"
     }
-    let { cols, docs } = sheetutil.constructDocs(sheetDoc, unformattedData)
-    let converted = sheetutil.convertValues(cols, docs, datatypes, { tz: "America/New_York" })
+    let { cols, docs } = constructDocs(sheetDoc, unformattedData)
+    let converted = convertValues(cols, docs, datatypes, { tz: "America/New_York" })
     expect(JSON.parse(JSON.stringify(converted.docs[0]))).toMatchObject({
       "String": "Hello",
       "Number": 3,
@@ -67,263 +42,6 @@ describe('Construct Docs', () => {
     })
   })
 })
-
-describe('Datetime', () => {
-  let fconv = null
-  let tz = "America/New_York"
-  beforeEach(async () => {
-    let converter = sheetutil.createConverter({
-      Date: "Datetime"
-    })
-    fconv = converter["Date"]
-  })
-  it ('should parse null value as null', async () => {
-    expect(fconv(null)).toBe(null)
-  })
-  it ('should parse excel date number format for a specific timezone', async () => {
-    let excel = 43195.52087962963 // "2018-04-05T12:30:04.000" (spreadsheet) 
-    let d = fconv(excel, { tz })
-    // d is luxon type which JSON serializes to ISO i.e. d.toISO()
-    //expect(JSON.stringify({d})).toEqual(JSON.stringify({d: "2018-04-05T12:30:04.000-04:00"}))
-    expect(JSON.stringify({d})).toEqual(JSON.stringify({d: "2018-04-05T16:30:04.000Z"}))
-  })
-  it ('should parse excel date number format for a specific timezone', async () => {
-    let tz2 = "America/Los_Angeles"
-    let excel = 43195.52087962963 // "2018-04-05T12:30:04.000" (spreadsheet)
-    let d = fconv(excel, { tz: tz2 })
-    // d is luxon type which JSON serializes to ISO i.e. d.toISO()
-    expect(JSON.stringify({d})).toEqual(JSON.stringify({d: "2018-04-05T19:30:04.000Z"}))
-  })
-  it ('should parse number 0 as the excel epoch date for a specific timezone', async () => {
-    dzero = fconv(0, { tz }) // 1899-12-30T00:00:00.000 (spreadsheet)
-    //expect(JSON.stringify({ d: dzero })).toEqual(JSON.stringify({ d: "1899-12-30T00:00:00.000-05:00" }))
-    expect(JSON.stringify({ d: dzero })).toEqual(JSON.stringify({ d: "1899-12-30T05:00:00.000Z" }))
-  })
-  it ('should parse a date string using a user specified format', async () => {
-    let str = "4/5/2018 12:30:04"
-    let fmt = 'M/d/yyyy hh:mm:ss'
-    d = fconv(str, { tz, fmt })
-    expect(JSON.stringify({d})).toEqual(JSON.stringify({d: "2018-04-05T12:30:04.000-04:00"}))
-  })
-  it ('should leave an ISO UTC date string in UTC timezone', async () => {
-    let str = "2017-04-24T23:39:16.718Z"
-    let d = fconv(str, { tz })
-    expect(JSON.stringify({d})).toEqual(JSON.stringify({d:str}))
-  })
-  it ('should leave an ISO + offset date string in the same offset', async () => {
-    let str = "2017-04-24T19:39:16.718-07:00"
-    let d = fconv(str, { tz })
-    expect(JSON.stringify({d})).toEqual(JSON.stringify({d:str}))
-  })
-  it ('should parse an ISO without offset in the specified timezone', async () => {
-    let str = "2017-04-24T19:39:16.718"
-    let d = fconv(str, { tz })
-    expect(JSON.stringify({d})).toEqual(JSON.stringify({ d: "2017-04-24T19:39:16.718-04:00" }))
-  })
-  it ('should parse an ISO date (no time) as a date plus zero time in specified timezone', async () => {
-    let str = "2017-04-24"
-    let d = fconv(str, { tz })
-    expect(JSON.stringify({d})).toEqual(JSON.stringify({d:"2017-04-24T00:00:00.000-04:00"}))
-  })
-})
-
-describe('Date', () => {
-  let fconv = null
-  let tz = "America/New_York"
-  beforeEach(async () => {
-    let converter = sheetutil.createConverter({
-      Date: "Date"
-    })
-    fconv = converter["Date"]
-  })
-  it ('should parse null value as null', async () => {
-    expect(fconv(null)).toBe(null)
-  })
-  it ('should parse excel date number format for a specific timezone', async () => {
-    let excel = 43195.52087962963 // "2018-04-05T12:30:04.000-04:00" 
-    let d = fconv(excel, { tz })
-    // d is luxon type which JSON serializes to ISO i.e. d.toISO()
-    expect(JSON.stringify({d})).toEqual(JSON.stringify({d: "2018-04-05T00:00:00.000Z"}))
-  })
-  it ('should parse number 0 as the excel epoch date for a specific timezone', async () => {
-    dzero = fconv(0, { tz })
-    expect(JSON.stringify({ d: dzero })).toEqual(JSON.stringify({ d: "1899-12-30T00:00:00.000Z" }))
-  })
-  it ('should parse a date string usinguser specified format', async () => {
-    let str = "4/5/2018 12:30:04"
-    let fmt = 'M/d/yyyy hh:mm:ss'
-    d = fconv(str, { tz, fmt })
-    expect(JSON.stringify({d})).toEqual(JSON.stringify({d: "2018-04-05T00:00:00.000Z"}))
-  })
-  it ('should leave an ISO UTC date string in UTC timezone', async () => {
-    let str = "2017-04-24T23:39:16.718Z"
-    let d = fconv(str, { tz })
-    expect(JSON.stringify({d})).toEqual(JSON.stringify({d: "2017-04-24T00:00:00.000Z"}))
-  })
-  it ('should leave an ISO + offset date string in the same offset', async () => {
-    let str = "2017-04-24T19:39:16.718-07:00"
-    let d = fconv(str, { tz })
-    expect(JSON.stringify({d})).toEqual(JSON.stringify({d: "2017-04-24T00:00:00.000Z"}))
-  })
-  it ('should parse an ISO without offset in the specified timezone', async () => {
-    let str = "2017-04-24T19:39:16.718"
-    let d = fconv(str, { tz })
-    expect(JSON.stringify({d})).toEqual(JSON.stringify({ d: "2017-04-24T00:00:00.000Z" }))
-  })
-  it ('should parse an ISO date (no time) as a date plus zero time in specified timezone', async () => {
-    let str = "2017-04-24"
-    let d = fconv(str, { tz })
-    expect(JSON.stringify({d})).toEqual(JSON.stringify({d:"2017-04-24T00:00:00.000Z"}))
-  })
-})
-
-describe('JSON', () => {
-  let fconv = null
-  beforeEach(async () => {
-    let converter = sheetutil.createConverter({
-      JSON: "JSON"
-    })
-    fconv = converter["JSON"]
-  })
-  it ('should parse null value as null', async () => {
-    expect(fconv(null)).toBe(null)
-  })
-  it ('should a valid JSON string', async () => {
-    let str = JSON.stringify({ hello: "world" })
-    let obj = fconv(str)
-    expect(obj).toEqual({ hello: "world" })
-  })
-  it ('should throw on invalid JSON string', async () => {
-    let invalid = 'invalid'
-    let error = null
-    try {
-      fconv(invalid)
-    } catch (err) {
-      error = err
-    }
-    expect(error).toBeTruthy()
-    expect(error.message).toEqual("Unexpected token i in JSON at position 0")
-  })
-  it ('should return a number when given a number', async () => {
-    let number = 1
-    expect(fconv(number)).toBe(1)
-  })
-  it ('should stringify and parse when given an object', async () => {
-    let arr = [ 1, 2 ]
-    let obj = fconv(arr)
-    expect(obj).toEqual([1, 2])
-    expect(obj === arr).toBe(false)
-  })
-})
-
-describe('StringList', () => {
-  let fconv = null
-  beforeEach(async () => {
-    let converter = sheetutil.createConverter({
-      StringList: "StringList"
-    })
-    fconv = converter["StringList"]
-  })
-  it ('should parse null as empty array', async () => {
-    expect(fconv(null)).toEqual([])
-  })
-  it ('should treat a single value as an array with one value', async () => {
-    let str = "One"
-    expect(fconv(str)).toEqual([ "One" ])
-  })
-  it ('should split on newline', async () => {
-    let str = `One\nTwo\nThree`
-    expect(fconv(str)).toEqual([ "One", "Two", "Three" ])
-  })
-  it ("should split on a custom separator", async () => {
-    let str = `One,Two,Three`
-    expect(fconv(str, { separator: ',' })).toEqual([ "One", "Two", "Three" ])
-  })
-  it ('should strip whitespace', async () => {
-    let str = ` One\t\n\tTwo`
-    expect(fconv(str)).toEqual([ "One", "Two" ])
-  })
-  it ('should filter all empty items', async () => {
-    let str = `\n\nOne\nTwo\n`
-    expect(fconv(str)).toEqual([ "One", "Two" ])
-  })
-  it ('should string and array a single number', async () => {
-    let number = 1
-    expect(fconv(number)).toEqual([ "1" ])
-  })
-  it ('should throw on any object', async () => {
-    let obj = { hello: "world" }
-    let error = null
-    try {
-      fconv(obj)
-    } catch (err) {
-      error = err
-    }
-    expect(error).toBeTruthy()
-    expect(error.message).toEqual("Could not convert value [object Object] to StringList")
-  })
-})
-
-describe('Number', () => {
-  let numconv = null
-  let intconv = null
-  let floatconv = null
-  beforeEach(async () => {
-    let converter = sheetutil.createConverter({
-      Number: "Number",
-      Int: "Int",
-      Float: "Float"
-    })
-    numconv = converter["Number"]
-    intconv = converter["Int"]
-    floatconv = converter["Float"]
-  })
-  it ('should parse a float as int', async () => {
-    expect(intconv(1.1)).toEqual(1)
-  })
-  it ('should parse a string as a int', async () => {
-    expect(intconv("1.1")).toEqual(1)
-  })
-  it ('should parse a string as a float', async () => {
-    expect(floatconv("1.1")).toEqual(1.1)
-  })
-})
-
-
-function createTestData() {
-  let docs = [ {
-    "col1": "val1a"
-  }, {
-    "col1": "val1b",
-    "col2": "val2b"
-  } ]
-
-  let cols = [ "col1", "col2", "col3" ]
-
-  return { cols, docs }
-}
-
-function createTestMetadata() {
-  let metadata = { }
-  metadata.sheets = [ {
-    title: "Sheet1",
-    columns: [ "A", "B", "C" ],
-    samples: {
-      "A": "A1",
-      "B": "B1",
-      "C": null
-    }
-  }, {
-    title: "Sheet2",
-    columns: [ "B", "D" ],
-    samples: {
-      "B": "B2",
-      "D": "D2"
-    }
-  } ]
-  return metadata
-}
-
 
 const formattedData = [
   [
