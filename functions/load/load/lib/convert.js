847679e371cf44fd73f19e969eff615b14fc96e4
Daniel Jhin Yoo <danieljyoo@gmail.com>
2019-08-30 23:05:53 -0700

    load refactor: moved conversion code into convert.js

---
+++ functions/load/load/lib/convert.js
@@ -0,0 +1,220 @@
+
+const { DateTime } = require('luxon')
+
+function convertValues(cols, docs, datatypes, options) {
+  let conv = createConverter(datatypes)
+  options = options || { }
+  for (let doc of docs) {
+    for (let col of cols) {
+      try {
+        if (conv[col]) {
+          doc[col] = conv[col](doc[col], options)
+        }
+      } catch (err) {
+        doc["_errors"].push({
+          col, message: err.message
+        })
+      }
+    }
+  }
+  return { cols, docs }
+}
+
+function createConverter(datatypes) {
+  let conv = { }
+  for (let col in datatypes) {
+    switch(datatypes[col]) {
+      case "Boolean":
+        conv[col] = convertToBoolean
+        break
+      case "String":
+        conv[col] = convertToString
+        break
+      case "Number":
+        conv[col] = convertToNumber
+        break
+      case "Int":
+        conv[col] = convertToInt
+        break
+      case "Float":
+        conv[col] = convertToNumber
+        break
+      case "Date":
+        conv[col] = convertToDate
+        break
+      case "Datetime":
+        conv[col] = convertToDatetime
+        break
+      case "JSON":
+        conv[col] = convertToJSON
+        break
+      case "StringList":
+        conv[col] = convertToStringList
+        break
+      case "GoogleDoc":
+        // we just noop because we'll have separate
+        // logic that will deal with Google Docs
+        conv[col] = convertNoop
+        break
+      default:
+        throw new Error(`Unknown type ${datatypes[col]}`)
+    }
+  }
+  return conv
+}
+
+const convertToString = (v) => {
+  if (!v) return ''
+  switch(typeof v) {
+    case "string":
+      return v
+    case "number":
+      return v.toString()
+    case "object":
+      return JSON.stringify(v)
+    default:
+      throw new Error(`Could not convert value ${v} to type String`)
+  }
+}
+
+const convertToNumber = (v) => {
+  if (!v) return 0
+  switch(typeof v) {
+    case "number": 
+      return v
+    case "string": 
+      return new Number(v)
+    default:
+      throw new Error(`Could not convert value ${v} to type Number`)
+  }
+}
+
+const convertToInt = (v) => {
+  if (!v) return 0
+  switch(typeof v) {
+    case "number": 
+      return Math.floor(v)
+    case "string": 
+      return parseInt(v)
+    default:
+      throw new Error(`Could not convert value ${v} to type Int`)
+  }
+}
+
+
+const convertToBoolean = (v) => {
+  if (!v) return false
+  return true
+}
+
+// what is the value of empty date cell? 
+// it doesn't seem to be null because 
+// we are throwing error. Need to test this.
+const convertToDatetime = (v, options) => {
+  options = options || { }
+  let tz = options.tz || "utc"
+  if (v === null) return null
+  switch(typeof v) {
+    case "number": 
+      return getISOStringFromExcel(v, tz)
+    case "string": 
+      let fmt = options.fmt || 'ISO' 
+      return parseDatetimeFromString(v, fmt, tz)
+    default:
+      throw new Error(`Could not convert value ${v} to type Date`)
+  }
+}
+
+// Will use the month, day, year and store in UTC WITHOUT tz converstion
+// For example, if the the sheet's timezone is
+// the value is the equivalent of '2009-04-01'('America/Los_Angeles') 
+// what will be stored is 'Z' (UTC)
+const convertToDate = (v, options) => {
+  options = options || { }
+  let tz = "utc"
+  if (v === null) return null
+  switch(typeof v) {
+    case "number": 
+      return getISODateStringFromExcel(v, tz)
+    case "string": 
+      let fmt = options.fmt || 'ISO' 
+      return parseDateFromString(v, fmt, tz)
+    default:
+      throw new Error(`Could not convert value ${v} to type Date`)
+  }
+}
+
+const convertToJSON = (v) => {
+  switch (typeof v) {
+    case "string":
+      return JSON.parse(v)
+    case "number": 
+      return JSON.parse(v)
+    case "object":
+      return JSON.parse(JSON.stringify(v))
+    default:
+      throw new Error(`Could not convert value ${v} to JSON`)
+  }
+}
+
+const convertToStringList = (v, options) => {
+  options = options || { }
+  let separator = options.separator || '\n'
+  if (!v) return [ ]
+  switch (typeof v) {
+    case "string":
+      return v.split(separator).map(s => s.trim()).filter(s => s)
+    case "number":
+      return [ v.toString() ]
+    default:
+      throw new Error(`Could not convert value ${v} to StringList`)
+  }
+}
+
+const convertNoop = (v) => {
+  return v
+}
+
+function getISOStringFromExcel(excelDate, tz) {
+  let d = getJsDateFromExcel(excelDate)
+  let str = d.toISOString().slice(0, -1) // remove 'Z' so we have a tz agnostic date
+  // return DateTime.fromISO(str, { zone: tz })
+  let datetime = DateTime.fromISO(str, { zone: tz })  // we have the literal time in the local timezone (the spreadsheet)
+  return datetime.setZone('utc') // we convert local time to utc
+}
+
+function getISODateStringFromExcel(excelDate, tz) {
+  let d = getJsDateFromExcel(excelDate)
+  let str = d.toISOString().split('T')[0] // 2018-04-05
+  return DateTime.fromISO(str, { zone: tz })
+}
+
+function getJsDateFromExcel(excelDate) { 
+  return new Date(Math.round((excelDate - (25567 + 2))*86400)*1000) 
+}
+
+function parseDatetimeFromString(str, fmt, tz) {
+  if (fmt == "ISO") {
+    // setZone only sets the zone if str does not include offset
+    return DateTime.fromISO(str, { zone: tz, setZone: true })
+  }
+  return DateTime.fromFormat(str, fmt, { zone: tz })
+}
+
+function parseDateFromString(str, fmt, tz) {
+  if (fmt == "ISO") {
+    // setZone only sets the zone if str does not include offset
+    str = str.split('T')[0]
+    return DateTime.fromISO(str, { zone: tz, setZone: true }).set({
+      hour: 0, minute: 0, seconds: 0, millisecond: 0 
+    })
+  }
+  return DateTime.fromFormat(str, fmt, { zone: tz }).set({
+    hour: 0, minute: 0, seconds: 0, millisecond: 0 
+  })
+}
+
+module.exports = {
+  convertValues,
+  createConverter,
+}
---
+++ functions/load/load/test/convert.test.js
@@ -0,0 +1,227 @@
+require('dotenv').config()
+const { DateTime } = require('luxon')
+
+const {
+  createConverter
+} = require('../lib/convert')
+
+describe('Datetime', () => {
+  let fconv = null
+  let tz = "America/New_York"
+  beforeEach(async () => {
+    let converter = createConverter({
+      Date: "Datetime"
+    })
+    fconv = converter["Date"]
+  })
+  it ('should parse null value as null', async () => {
+    expect(fconv(null)).toBe(null)
+  })
+  it ('should parse excel date number format for a specific timezone', async () => {
+    let excel = 43195.52087962963 // "2018-04-05T12:30:04.000" (spreadsheet) 
+    let d = fconv(excel, { tz })
+    // d is luxon type which JSON serializes to ISO i.e. d.toISO()
+    //expect(JSON.stringify({d})).toEqual(JSON.stringify({d: "2018-04-05T12:30:04.000-04:00"}))
+    expect(JSON.stringify({d})).toEqual(JSON.stringify({d: "2018-04-05T16:30:04.000Z"}))
+  })
+  it ('should parse excel date number format for a specific timezone', async () => {
+    let tz2 = "America/Los_Angeles"
+    let excel = 43195.52087962963 // "2018-04-05T12:30:04.000" (spreadsheet)
+    let d = fconv(excel, { tz: tz2 })
+    // d is luxon type which JSON serializes to ISO i.e. d.toISO()
+    expect(JSON.stringify({d})).toEqual(JSON.stringify({d: "2018-04-05T19:30:04.000Z"}))
+  })
+  it ('should parse number 0 as the excel epoch date for a specific timezone', async () => {
+    dzero = fconv(0, { tz }) // 1899-12-30T00:00:00.000 (spreadsheet)
+    //expect(JSON.stringify({ d: dzero })).toEqual(JSON.stringify({ d: "1899-12-30T00:00:00.000-05:00" }))
+    expect(JSON.stringify({ d: dzero })).toEqual(JSON.stringify({ d: "1899-12-30T05:00:00.000Z" }))
+  })
+  it ('should parse a date string using a user specified format', async () => {
+    let str = "4/5/2018 12:30:04"
+    let fmt = 'M/d/yyyy hh:mm:ss'
+    d = fconv(str, { tz, fmt })
+    expect(JSON.stringify({d})).toEqual(JSON.stringify({d: "2018-04-05T12:30:04.000-04:00"}))
+  })
+  it ('should leave an ISO UTC date string in UTC timezone', async () => {
+    let str = "2017-04-24T23:39:16.718Z"
+    let d = fconv(str, { tz })
+    expect(JSON.stringify({d})).toEqual(JSON.stringify({d:str}))
+  })
+  it ('should leave an ISO + offset date string in the same offset', async () => {
+    let str = "2017-04-24T19:39:16.718-07:00"
+    let d = fconv(str, { tz })
+    expect(JSON.stringify({d})).toEqual(JSON.stringify({d:str}))
+  })
+  it ('should parse an ISO without offset in the specified timezone', async () => {
+    let str = "2017-04-24T19:39:16.718"
+    let d = fconv(str, { tz })
+    expect(JSON.stringify({d})).toEqual(JSON.stringify({ d: "2017-04-24T19:39:16.718-04:00" }))
+  })
+  it ('should parse an ISO date (no time) as a date plus zero time in specified timezone', async () => {
+    let str = "2017-04-24"
+    let d = fconv(str, { tz })
+    expect(JSON.stringify({d})).toEqual(JSON.stringify({d:"2017-04-24T00:00:00.000-04:00"}))
+  })
+})
+
+describe('Date', () => {
+  let fconv = null
+  let tz = "America/New_York"
+  beforeEach(async () => {
+    let converter = createConverter({
+      Date: "Date"
+    })
+    fconv = converter["Date"]
+  })
+  it ('should parse null value as null', async () => {
+    expect(fconv(null)).toBe(null)
+  })
+  it ('should parse excel date number format for a specific timezone', async () => {
+    let excel = 43195.52087962963 // "2018-04-05T12:30:04.000-04:00" 
+    let d = fconv(excel, { tz })
+    // d is luxon type which JSON serializes to ISO i.e. d.toISO()
+    expect(JSON.stringify({d})).toEqual(JSON.stringify({d: "2018-04-05T00:00:00.000Z"}))
+  })
+  it ('should parse number 0 as the excel epoch date for a specific timezone', async () => {
+    dzero = fconv(0, { tz })
+    expect(JSON.stringify({ d: dzero })).toEqual(JSON.stringify({ d: "1899-12-30T00:00:00.000Z" }))
+  })
+  it ('should parse a date string usinguser specified format', async () => {
+    let str = "4/5/2018 12:30:04"
+    let fmt = 'M/d/yyyy hh:mm:ss'
+    d = fconv(str, { tz, fmt })
+    expect(JSON.stringify({d})).toEqual(JSON.stringify({d: "2018-04-05T00:00:00.000Z"}))
+  })
+  it ('should leave an ISO UTC date string in UTC timezone', async () => {
+    let str = "2017-04-24T23:39:16.718Z"
+    let d = fconv(str, { tz })
+    expect(JSON.stringify({d})).toEqual(JSON.stringify({d: "2017-04-24T00:00:00.000Z"}))
+  })
+  it ('should leave an ISO + offset date string in the same offset', async () => {
+    let str = "2017-04-24T19:39:16.718-07:00"
+    let d = fconv(str, { tz })
+    expect(JSON.stringify({d})).toEqual(JSON.stringify({d: "2017-04-24T00:00:00.000Z"}))
+  })
+  it ('should parse an ISO without offset in the specified timezone', async () => {
+    let str = "2017-04-24T19:39:16.718"
+    let d = fconv(str, { tz })
+    expect(JSON.stringify({d})).toEqual(JSON.stringify({ d: "2017-04-24T00:00:00.000Z" }))
+  })
+  it ('should parse an ISO date (no time) as a date plus zero time in specified timezone', async () => {
+    let str = "2017-04-24"
+    let d = fconv(str, { tz })
+    expect(JSON.stringify({d})).toEqual(JSON.stringify({d:"2017-04-24T00:00:00.000Z"}))
+  })
+})
+
+describe('JSON', () => {
+  let fconv = null
+  beforeEach(async () => {
+    let converter = createConverter({
+      JSON: "JSON"
+    })
+    fconv = converter["JSON"]
+  })
+  it ('should parse null value as null', async () => {
+    expect(fconv(null)).toBe(null)
+  })
+  it ('should a valid JSON string', async () => {
+    let str = JSON.stringify({ hello: "world" })
+    let obj = fconv(str)
+    expect(obj).toEqual({ hello: "world" })
+  })
+  it ('should throw on invalid JSON string', async () => {
+    let invalid = 'invalid'
+    let error = null
+    try {
+      fconv(invalid)
+    } catch (err) {
+      error = err
+    }
+    expect(error).toBeTruthy()
+    expect(error.message).toEqual("Unexpected token i in JSON at position 0")
+  })
+  it ('should return a number when given a number', async () => {
+    let number = 1
+    expect(fconv(number)).toBe(1)
+  })
+  it ('should stringify and parse when given an object', async () => {
+    let arr = [ 1, 2 ]
+    let obj = fconv(arr)
+    expect(obj).toEqual([1, 2])
+    expect(obj === arr).toBe(false)
+  })
+})
+
+describe('StringList', () => {
+  let fconv = null
+  beforeEach(async () => {
+    let converter = createConverter({
+      StringList: "StringList"
+    })
+    fconv = converter["StringList"]
+  })
+  it ('should parse null as empty array', async () => {
+    expect(fconv(null)).toEqual([])
+  })
+  it ('should treat a single value as an array with one value', async () => {
+    let str = "One"
+    expect(fconv(str)).toEqual([ "One" ])
+  })
+  it ('should split on newline', async () => {
+    let str = `One\nTwo\nThree`
+    expect(fconv(str)).toEqual([ "One", "Two", "Three" ])
+  })
+  it ("should split on a custom separator", async () => {
+    let str = `One,Two,Three`
+    expect(fconv(str, { separator: ',' })).toEqual([ "One", "Two", "Three" ])
+  })
+  it ('should strip whitespace', async () => {
+    let str = ` One\t\n\tTwo`
+    expect(fconv(str)).toEqual([ "One", "Two" ])
+  })
+  it ('should filter all empty items', async () => {
+    let str = `\n\nOne\nTwo\n`
+    expect(fconv(str)).toEqual([ "One", "Two" ])
+  })
+  it ('should string and array a single number', async () => {
+    let number = 1
+    expect(fconv(number)).toEqual([ "1" ])
+  })
+  it ('should throw on any object', async () => {
+    let obj = { hello: "world" }
+    let error = null
+    try {
+      fconv(obj)
+    } catch (err) {
+      error = err
+    }
+    expect(error).toBeTruthy()
+    expect(error.message).toEqual("Could not convert value [object Object] to StringList")
+  })
+})
+
+describe('Number', () => {
+  let numconv = null
+  let intconv = null
+  let floatconv = null
+  beforeEach(async () => {
+    let converter = createConverter({
+      Number: "Number",
+      Int: "Int",
+      Float: "Float"
+    })
+    numconv = converter["Number"]
+    intconv = converter["Int"]
+    floatconv = converter["Float"]
+  })
+  it ('should parse a float as int', async () => {
+    expect(intconv(1.1)).toEqual(1)
+  })
+  it ('should parse a string as a int', async () => {
+    expect(intconv("1.1")).toEqual(1)
+  })
+  it ('should parse a string as a float', async () => {
+    expect(floatconv("1.1")).toEqual(1.1)
+  })
+})
