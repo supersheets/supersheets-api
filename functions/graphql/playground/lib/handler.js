128153b95c86155c91540b5aa247b07d25d8577f
Daniel Jhin Yoo <danieljyoo@gmail.com>
2019-10-24 22:04:16 -0700

    updated default query in graphql playground to use rows row rather than nodes edges

--- functions/graphql/playground/lib/handler.js
+++ functions/graphql/playground/lib/handler.js
@@ -125,8 +125,8 @@ const DEFAULT_QUERY = `
 # return _id, _row, and _sheet for all records with _row less than 10
 query {
   find(filter: { _row: { lt: 10 } }) {
-    edges {
-      node {
+    rows {
+      row {
         _id
         _row
         _sheet
